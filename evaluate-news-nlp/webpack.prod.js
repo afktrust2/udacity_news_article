ddaa1be926805238a04e173ec10c0328f26b69bf
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2021-05-13 15:26:49 -0700

    edits from lesson

--- evaluate-news-nlp/webpack.prod.js
+++ evaluate-news-nlp/webpack.prod.js
@@ -1,10 +1,18 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+               minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({}), new MiniCssExtractPlugin({ filename: "main.css" }), new WorkboxPlugin.GenerateSW()],
+                  },
     output: {
                libraryTarget: 'var',
                library: 'Client'
@@ -18,7 +26,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             }
         ]
     },
