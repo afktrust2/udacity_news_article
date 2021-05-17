520c7b42fffa392f9e448cfb10bfa966c08df8b3
Trent Ikehara <76722415+afktrust2@users.noreply.github.com>
2021-05-17 14:38:20 -0700

    test file

--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -2,11 +2,19 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const APIKey = process.env.API_KEY;
+const baseURl = 'https://api.meaningcloud.com/sentiment-2.1'
+
 const app = express()
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+console.log(`Your API key is ${process.env.API_KEY}`);
+
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
